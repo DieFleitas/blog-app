a75c5099f65f1547c547c95b830a8d41c1e46f2c
DieFleitas <diegofleitas98@outlook.es>
2022-09-30 12:08:03 -0300

    joining mysql tables

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -5,14 +5,23 @@ export const getPosts = (req, res) => {
     ? "SELECT * FROM posts WHERE cat=?"
     : "SELECT * FROM posts";
 
-    db.query(q, [req.query.cat], (err, data) => {
-        if(err) return res.send(err)
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.send(err);
 
-        return res.status(200).json(data)
-    })
+    return res.status(200).json(data);
+  });
 };
 
-export const getPost = (req, res) => {};
+export const getPost = (req, res) => {
+  const q =
+    "SELECT `username`, `title`, `desc`, p.img, u.img AS userImage, `cat`, `date` FROM users u JOIN posts p ON u.id=p.uid WHERE p.id=?";
+
+    db.query(q, [req.params.id], (err, data) => {
+        if(err) return res.json(err)
+
+        return res.status(200).json(data[0])
+    })
+};
 
 export const addPost = (req, res) => {};
 
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
 import Edit from "../img/edit.png";
 import Delete from "../img/edit.png";
@@ -11,6 +11,7 @@ const Single = () => {
   const [post, setPost] = useState({});
 
   const location = useLocation();
+  const navigate = useNavigate()
 
   const postId = location.pathname.split("/")[2];
 
@@ -28,12 +29,21 @@ const Single = () => {
     fetchData();
   }, [postId]);
 
+  const handleDelete = async () => {
+    try {
+       await axios.delete(`/posts/${postId}`);
+      Navigate('/')
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <div className="single">
       <div className="content">
         <img src={post?.img} alt="" />
         <div className="user">
-          <img src="" alt="" />
+          {post.userImage && <img src={post.userImage} alt="" />}
           <div className="info">
             <span>{post.username}</span>
             <p>Posted {moment(post.date).fromNow}</p>
@@ -43,7 +53,7 @@ const Single = () => {
               <Link to={`/write?edit=2`}>
                 <img src={Edit} alt="" />
               </Link>
-              <img src={Delete} alt="" />
+              <img onClick={handleDelete} src={Delete} alt="" />
             </div>
           )}
         </div>
